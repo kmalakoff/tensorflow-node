15ff0d7e976e013811a2c5c6b340aa5cf154ab0a
Kevin Malakoff <kmalakoff@gmail.com>
2016-12-08 20:39:23 -0800

    Trying out javascript APIs

--- src/core/graph.js
+++ src/core/graph.js
@@ -1,3 +1,11 @@
 const _tf = require('../nan_addons');
 
-module.exports = _tf.Graph;
+module.exports = class Graph {
+  constructor() { this._ = new _tf.Graph(); }
+
+  input() { return this._.input(); }
+  constant(v) { return this._.constant(v); }
+  add(l, r) { return this._.add(l, r); }
+  matmul(l, r) { return this._.matmul(l, r); }
+  run(ops, input_pairs) { return this._.run(ops, input_pairs); }
+};
