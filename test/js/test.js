0fbeabd2ff6bb5fff51bebce23f75eb47c3f315b
Kevin Malakoff <kmalakoff@gmail.com>
2016-12-12 09:56:27 -0800

    Reenable tests

--- test/js/test.js
+++ test/js/test.js
@@ -1,9 +1,9 @@
-// const assert = require('chai').assert;
-// const tf = require('../../index');
+const assert = require('chai').assert;
+const tf = require('../../index');
 
-// describe("Tensorflow Test", function() {
+describe("Tensorflow Test", function() {
 
-//   it("has expected types", function() {
-//     (new tf.Test()).run();
-//   });
-// });
+  it("has expected types", function() {
+    (new tf.Test()).run();
+  });
+});
